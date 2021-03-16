caa5b838c5c8024d2857b9e0cdf8a6e29a19d554
dedi27 <dedi27@gmail.com>
2021-03-16 14:04:38 -0300

    Create sendS3.sh

---
+++ sendS3.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+file="$1"
+key_id="<s3_access_key>"
+key_secret="<s3_secret_key>"
+path="$file"
+bucket="<bucket>"
+content_type="application/octet-stream"
+date="$(LC_ALL=C date -u +"%a, %d %b %Y %X %z")"
+md5="$(openssl md5 -binary < "$file" | base64)"
+
+sig="$(printf "PUT\n$md5\n$content_type\n$date\n/$bucket/$path" | openssl sha1 -binary -hmac "$key_secret" | base64)"
+
+curl -v -T $file https://$bucket.s3.portal-tio.unifique-hd.net/$path \
+    -H "Date: $date" \
+    -H "Authorization: AWS $key_id:$sig" \
+    -H "Content-Type: $content_type" \
+    -H "Content-MD5: $md5"
